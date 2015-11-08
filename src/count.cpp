1fcc62c6f2fe7fda24043148dd37c3de26482e0f
kathariakhila <kathariakhila@gmail.com>
2015-11-08 13:52:31 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,30 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+	
+	int i = num1;
+	
+	int count = 0;
+	
+	if (num1 == 0 && num2 != 0)
+		return -1;
+	else if (num1 < 0 && num2 >0)
+		return -1;
+	else if (num1 > 0 && num2 == 0)
+		return 0;
+	else if (num1 < 0 && num2 == 0)
+		return 0;
+
+	else{
+		while (i <= num2){
+			count++;
+			i = i + num1;
+		}
+
+		return count;
+
+	}
+
+	
+
 }
